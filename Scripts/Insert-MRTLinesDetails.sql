6e6911b1879b38f0d94f849f93d03003b0cd2152
rhyli02 <48325632+rhyli02@users.noreply.github.com>
2020-09-25 23:20:31 +0800

    Script for Q-Less DATA
    execute both script

---
+++ Scripts/Insert-MRTLinesDetails.sql
@@ -0,0 +1,44 @@
+USE [QLessDB]
+GO
+
+INSERT INTO [dbo].[MRTLineDetails]
+           ([MRTLine]
+		   ,[MRTLineCode]
+           ,[MRTLineDescription]
+           ,[Status])
+     VALUES
+           ('MRTLine1','MRTL1-001','BACLARAN','Active'),
+		   ('MRTLine1','MRTL1-002','EDSA','Active'),
+		   ('MRTLine1','MRTL1-003','LIBERTAD','Active'),
+		   ('MRTLine1','MRTL1-004','GIL PUYAT','Active'),
+		   ('MRTLine1','MRTL1-005','V.CRUZ','Active'),
+		   ('MRTLine1','MRTL1-006','QUIRINO','Active'),
+		   ('MRTLine1','MRTL1-007','P.GIL','Active'),
+		   ('MRTLine1','MRTL1-008','UNITED NATION','Active'),
+		   ('MRTLine1','MRTL1-009','C.TERMINAL','Active'),
+		   ('MRTLine1','MRTL1-010','CARRIEDO','Active'),
+		   ('MRTLine1','MRTL1-011','D.JOSE','Active'),
+		   ('MRTLine1','MRTL1-012','BAMBANG','Active'),
+		   ('MRTLine1','MRTL1-013','TAYUMAN','Active'),
+		   ('MRTLine1','MRTL1-014','BLUMENTRITT','Active'),
+		   ('MRTLine1','MRTL1-015','A.SANTOS','Active'),
+		   ('MRTLine1','MRTL1-016','R.PAPA','Active'),
+		   ('MRTLine1','MRTL1-017','5TH AVE','Active'),
+		   ('MRTLine1','MRTL1-018','MONUMENTO','Active'),
+		   ('MRTLine1','MRTL1-019','BALINTAWAK','Active'),
+		   ('MRTLine1','MRTL1-020','ROOSEVELT','Active'),
+
+		   ('MRTLine2','MRTL2-001','RECTO','Active'),
+		   ('MRTLine2','MRTL2-002','LEGARDA','Active'),
+		   ('MRTLine2','MRTL2-003','PUREZA','Active'),
+		   ('MRTLine2','MRTL2-004','V.MAPA','Active'),
+		   ('MRTLine2','MRTL2-005','J.RUIZ','Active'),
+		   ('MRTLine2','MRTL2-006','GILMORE','Active'),
+		   ('MRTLine2','MRTL2-007','BETTY-GO','Active'),
+		   ('MRTLine2','MRTL2-008','CUBAO','Active'),
+		   ('MRTLine2','MRTL2-009','ANONAS','Active'),
+		   ('MRTLine2','MRTL2-010','KATIPUNAN','Active'),
+		   ('MRTLine2','MRTL2-011','SANTOLAN','Active')
+GO
+
+
